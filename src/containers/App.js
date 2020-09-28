6c90ac708247d1616c4202925b7b3419b5530cc9
wojdanowski <s.wojdanowski@gmail.com>
2020-09-28 22:23:18 +0200

    uiReducer setup

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './app.scss';
 import Main from './MainPage/MainPage';
 import Layout from './../hoc/Layout/Layout';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import uiReducer from '../store/reducers/uiReducer';
+
+const store = createStore(uiReducer);
 
 function App() {
 	return (
-		<Layout>
-			<Main />
-		</Layout>
+		<Provider store={store}>
+			<Layout>
+				<Main />
+			</Layout>
+		</Provider>
 	);
 }
 
