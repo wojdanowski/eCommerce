b025fffcefd2445df1155f4ea5df9771cd78aaa9
wojdanowski <s.wojdanowski@gmail.com>
2020-09-29 23:20:03 +0200

    main manu items to icons

--- src/components/UI/Navigation/MainNav/MainNav.js
+++ src/components/UI/Navigation/MainNav/MainNav.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import classes from './MainNav.module.scss';
 import MainNavItem from './MainNavItem/MainNavItem';
+import { RiShoppingBasket2Line, RiUser3Line } from 'react-icons/ri';
+import IconButton from './../../Buttons/IconButton/IconButton';
 
 const MainNav = (props) => {
+	let appendClass = '';
+
+	props.direction === 'vertical'
+		? (appendClass = classes.vertical)
+		: (appendClass = '');
+
 	return (
-		<div className={classes.navItems}>
+		<div className={`${classes.navItems} ${appendClass}`}>
 			<ul>
-				<MainNavItem link='index.html' label='Home' isCurrent={true} />
 				<MainNavItem
 					link='index.html'
-					label='Basket'
+					label='Home'
+					isCurrent={true}
+					direction={props.direction}
+				/>
+				<IconButton
 					clicked={props.toggleRightSidebar}
+					icon={<RiShoppingBasket2Line />}
+				/>
+				<IconButton
+					clicked={props.toggleRightSidebar}
+					icon={<RiUser3Line />}
 				/>
 			</ul>
 		</div>
