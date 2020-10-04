c9d45866a613bcbe1e46590df8df323446a943a5
wojdanowski <s.wojdanowski@gmail.com>
2020-10-05 00:45:09 +0200

    fetch hook impl

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -4,17 +4,27 @@ import Aux from '../../hoc/Auxiliary/Auxiliary';
 import MainHeader from './../../components/MainHeader/MainHeader';
 import CategoriesBar from '../CategoriesBar/CategoriesBar';
 import AllProdCards from './../../components/ProductCards/AllProdCards';
+import useDataApi from './../../hooks/useFetchData';
 
 const Main = (props) => {
+	const [{ data, isLoading, isError }, doFetch] = useDataApi(
+		'https://ecommerceprodmockup.firebaseio.com/products.json',
+		{
+			hits: [],
+		}
+	);
+
+	const loadingScreen = <div> Loading... </div>;
+
 	return (
 		<Aux>
 			<MainHeader />
 			<div className='utilContainer'>
 				<CategoriesBar />
 			</div>
-
+			{isError && <div>Something went wrong ...</div>}
 			<div className='utilBigContainer'>
-				<AllProdCards />
+				{isLoading ? loadingScreen : <AllProdCards prodData={data} />}
 			</div>
 		</Aux>
 	);
