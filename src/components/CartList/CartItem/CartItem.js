9a639d0ba33520b7595860ad3fd44be6a8c21440
wojdanowski <s.wojdanowski@gmail.com>
2020-10-04 18:19:09 +0200

    cart item img and description added

--- src/components/CartList/CartItem/CartItem.js
+++ src/components/CartList/CartItem/CartItem.js
@@ -4,15 +4,18 @@ import prodImg from '../../../assets/img/product.jpg';
 
 const CartItem = (props) => {
 	return (
-		<div className={classes.cartItemContainer}>
-			<img className={classes.prodImg} src={prodImg} alt='prodImg' />
+		<div className={`${classes.cartItemContainer}`}>
+			<div className={classes.prodImgContainer}>
+				<img className={classes.prodImg} src={prodImg} alt='prodImg' />
+			</div>
+
 			<div className={classes.prodDescription}>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
 					Quam id, officia quas ipsa suscipit ipsam.
 				</p>
+				<div className={classes.cartItemRemove}>REMOVE</div>
 			</div>
-			<div className={classes.cartItemRemove}>REMOVE</div>
 		</div>
 	);
 };
