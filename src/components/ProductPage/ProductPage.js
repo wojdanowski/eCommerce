084816779c469d407b0c967df30c5cfdc7e8a74c
wojdanowski <s.wojdanowski@gmail.com>
2020-10-05 22:40:59 +0200

    modal and backdrop basic imp

---
+++ src/components/ProductPage/ProductPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './ProductPage.module.scss';
+
+const ProductPage = (props) => {
+	return (
+		<div className={classes.prodPageContainer}>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Similique dolores amet harum quos non reiciendis, commodi illum
+				consequuntur ipsam ut.
+			</p>
+		</div>
+	);
+};
+
+export default ProductPage;
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import classes from './Modal.module.scss';
+import Backdrop from './../Backdrop/Backdrop';
+
+const Modal = (props) => {
+	return (
+		<Fragment>
+			<Backdrop show={props.show} clicked={props.modalClosed} />
+			<div
+				className={classes.Modal}
+				style={{
+					transform: props.show
+						? 'translateY(0)'
+						: 'translateY(-100vh)',
+					opacity: props.show ? '1' : '0',
+				}}
+			>
+				{props.children}
+			</div>
+		</Fragment>
+	);
+};
+
+export default Modal;
