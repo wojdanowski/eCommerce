a0f2734a4b14d24f36a03f8511c7c449f2fa4418
wojdanowski <s.wojdanowski@gmail.com>
2020-11-07 15:09:05 +0100

    node version changed to 8, basic endpoint ready

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,15 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+	if (req.method !== 'POST') {
+		return res.status(500).json({
+			message: 'Not allowed',
+		});
+	}
+
+	res.status(200).json({
+		message: 'It worked!',
+	});
+});
