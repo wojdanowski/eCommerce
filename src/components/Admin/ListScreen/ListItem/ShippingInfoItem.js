caa47e93caee3b20e021e059a162171600a02a56
wojdanowski <s.wojdanowski@gmail.com>
2020-11-13 13:28:31 +0100

    OrderDetailsPage scss mobile adjustments

--- src/components/Admin/ListScreen/ListItem/ShippingInfoItem.js
+++ src/components/Admin/ListScreen/ListItem/ShippingInfoItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ListItem from './ListItem';
+import classes from './ShippingInfoItem.module.scss';
 
 const ShippingInfoItem = (props) => {
 	return (
 		<ListItem>
-			<p>{Object.keys(props.itemData)[0]}</p>
-			<p>{Object.values(props.itemData)[0]}</p>
+			<div className={classes.shippingContainer}>
+				<p>{Object.keys(props.itemData)[0]}</p>
+				<p>{Object.values(props.itemData)[0]}</p>
+			</div>
 		</ListItem>
 	);
 };
