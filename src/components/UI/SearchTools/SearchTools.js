b0c8e29a16bbbbf9851458e85c6f53046743d66f
wojdanowski <s.wojdanowski@gmail.com>
2020-11-18 11:11:43 +0100

    seatchBar clean up and redux setup for searchBar

--- src/components/UI/SearchTools/SearchTools.js
+++ src/components/UI/SearchTools/SearchTools.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import classes from './SearchTools.module.scss';
+import * as formActions from './../../../store/actions/formActions';
+import Input from './../Input/Input';
 
-const SearchTools = () => {
+const SearchTools = (props) => {
+	const formElementsArray = [];
+
+	for (let key in props.formFields) {
+		formElementsArray.push({
+			id: key,
+			config: props.formFields[key],
+		});
+	}
+
 	return (
-		<div className={classes.searchSort}>
-			<div className={classes.verticalEvenItem}>
-				<input type='text' placeholder='Search...' />
-			</div>
-			<div className={classes.verticalEvenItem}>
+		<div className={classes.searchBarContainer}>
+			{formElementsArray.map((formElement) => (
+				<Input
+					key={formElement.id}
+					elementType={formElement.config.elementType}
+					elementConfig={formElement.config.elementConfig}
+					value={formElement.config.value}
+					invalid={!formElement.config.valid}
+					shouldValidate={formElement.config.validation}
+					touched={formElement.config.touched}
+					changed={(event) =>
+						props.updateFormField(
+							event.target.value,
+							formElement.id,
+							'search'
+						)
+					}
+				/>
+			))}
+
+			{/* <div className={classes.verticalEvenItem}>
 				<form action=''>
 					<select name='' id=''>
 						<option value='A-Z'>A-Z</option>
@@ -20,9 +48,27 @@ const SearchTools = () => {
 			<div className={`${classes.verticalEvenItem} ${classes.noBorder}`}>
 				<label htmlFor=''>Tylko dostepne:</label>
 				<input type='checkbox' />
-			</div>
+			</div> */}
 		</div>
 	);
 };
 
-export default SearchTools;
+const mapStateToProps = (state) => {
+	return {
+		formFields: state.formState.searchForm.fields,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		updateFormField: (enteredValue, selectedInputId, form) =>
+			dispatch({
+				type: formActions.UPDATE_FIELD,
+				newValue: enteredValue,
+				inputId: selectedInputId,
+				form: form,
+			}),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchTools);
---
+++ src/store/reducers/formFields/searchBarFields.js
@@ -0,0 +1,17 @@
+const authFormFields = {
+	email: {
+		elementType: 'input',
+		elementConfig: {
+			type: 'text',
+			placeholder: 'Search',
+		},
+		value: '',
+		validation: {
+			required: false,
+		},
+		// valid: false,
+		touched: false,
+	},
+};
+
+export default authFormFields;
