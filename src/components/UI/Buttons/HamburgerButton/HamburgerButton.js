af1df4ec788e208aa69b5bd4f784273a14bbdcfb
wojdanowski <s.wojdanowski@gmail.com>
2020-11-18 15:19:11 +0100

    hambrger visibility

--- src/components/UI/Buttons/HamburgerButton/HamburgerButton.js
+++ src/components/UI/Buttons/HamburgerButton/HamburgerButton.js
@@ -2,8 +2,12 @@ import React from 'react';
 import classes from './HamburgerButton.module.scss';
 
 const HamburgerButton = (props) => {
+	const visibility = props.visible ? null : 'invisible';
 	return (
-		<div className={classes.hamburgerContainer} onClick={props.clicked}>
+		<div
+			className={`${classes.hamburgerContainer} ${visibility}`}
+			onClick={props.clicked}
+		>
 			<div></div>
 			<div></div>
 			<div></div>
